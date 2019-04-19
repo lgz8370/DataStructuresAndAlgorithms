8eb4284f115d03f4b9d6e6b7baad4fc3348978bb
LGZ <lgz_silent@163.com>
2019-04-19 17:56:42 +0800

    初始化项目

---
+++ leetcode/src/main/java/链表/_203_移除链表元素.java
@@ -0,0 +1,15 @@
+package 链表;
+
+/**
+ * @author LuGuoZheng
+ * @date 2019/4/19 16:49
+ * @Version 3.0
+ * @description https://leetcode-cn.com/problems/remove-linked-list-elements/
+ */
+public class _203_移除链表元素 {
+
+    public ListNode removeElements(ListNode head, int val) {
+
+        return null;
+    }
+}
